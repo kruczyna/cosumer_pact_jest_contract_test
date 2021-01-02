ceb85790c94bd96705dee95b8677a5c2f5c5f324
Victoria Kruczek <victoria.kruczek@gmail.com>
2021-01-02 18:50:09 +0100

    Added test for single kitten request

--- __tests__/contract/consumer/kittens.spec.js
+++ __tests__/contract/consumer/kittens.spec.js
@@ -1,7 +1,6 @@
 "use strict"
 
-const { Matchers } = require("@pact-foundation/pact")
-const { getKittens } = require("../../../src/consumer")
+const { getKittens, getKitten } = require("../../../src/kittenAPI")
 const availableKittens = require("../../../src/kittenData.js")
 
 
@@ -27,7 +26,7 @@ describe("Clients Service", () => {
                     headers: {
                         "Content-Type": "application/json; charset=utf-8",
                     },
-                    body: availableKittens,
+                    body: GET_EXPECTED_BODY,
                 },
             }
             return provider.addInteraction(interaction)
@@ -35,6 +34,44 @@ describe("Clients Service", () => {
 
         test("getKittens() returns expected body, header and statusCode", async() => {
             const response = await getKittens()
+            expect(response.headers['content-type']).toBe("application/json; charset=utf-8")
+            expect(response.data).toEqual(GET_EXPECTED_BODY)
+            expect(response.status).toEqual(200)
+        })
+    })
+
+    describe("GET Single kitten", () => {
+        const kittenID = 1
+        const GET_EXPECTED_BODY = [{
+            id: 1,
+            name: 'Zepsuszek',
+            furColor: 'black and white'
+          }]
+
+          beforeEach(() => {
+            const interaction = {
+                state: "i have a list of kittens",
+                uponReceiving: "get kitten with specific id",
+                withRequest: {
+                    method: "GET",
+                    path: `/kittens/${kittenID}`,
+                    headers: {
+                        Accept: "application/json, text/plain, */*",
+                    },
+                },
+                willRespondWith: {
+                    status: 200,
+                    headers: {
+                        "Content-Type": "application/json; charset=utf-8",
+                    },
+                    body: GET_EXPECTED_BODY,
+                },
+            }
+            return provider.addInteraction(interaction)
+        })
+
+        test("getKitten() returns expected body, header and statusCode", async() => {
+            const response = await getKitten(kittenID)
             expect(response.headers['content-type']).toBe("application/json; charset=utf-8")
             expect(response.data).toEqual(GET_EXPECTED_BODY)
             expect(response.status).toEqual(200)
